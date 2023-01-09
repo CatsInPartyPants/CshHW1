6aaccfd96d1d994f32cbf95d2eda842f421969cd
Artem Nilov <nilov1235@mail.ru>
2023-01-09 17:24:30 +0300

    Add project files.

---
+++ CshHW1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CshHW1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //task 1
+            int[] A = new int[5];
+            int[,] B = new int[3,4];
+
+            for(int i = 0; i < A.Length; i++)
+            {
+                A[i] = int.Parse(Console.ReadLine());
+            }
+            
+            for(int i = 0; i < B.)
+
+        }
+    }
+}
