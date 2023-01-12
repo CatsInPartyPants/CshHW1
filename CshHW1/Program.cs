510b249c97a9ee9297ccdd537f5f813a5901c7de
Artem Nilov <nilov1235@mail.ru>
2023-01-12 12:52:20 +0300

    1-4

--- CshHW1/Program.cs
+++ CshHW1/Program.cs
@@ -13,7 +13,7 @@ namespace CshHW1
 
             //task 1
             Random r = new Random();
-            /*
+            
             int[] A = new int[5];
             int[][] B = new int[3][];
             
@@ -87,13 +87,22 @@ namespace CshHW1
             Console.WriteLine($"Max in B is {maxInB}, Min in B is {minInB}, summ of array is {summB}, multiplication of elements in B is {multB}, summ of odd numbers {summOfOdd}");
 
             Console.ReadKey();
-           */
+           
+
+
             //task 2
 
+
+            
             int minPos = 0;
+            int minEl = 0;
             int maxPos = 0;
+            int maxEl = 0;
+            int summEl = 0;
 
             int[,] matrix2 = new int[5, 5];
+            int[] temp = new int[25];
+            int tempi = 0;
 
             for(int i =0; i < matrix2.GetLength(0); i++)
             {
@@ -101,13 +110,212 @@ namespace CshHW1
                 {
                     matrix2[i, j] = r.Next(-100, 100);
                     Console.Write($"{matrix2[i, j]} ");
-                    if()
                 }
                 Console.WriteLine();
             }
 
+
+            for(int i =0; i < matrix2.GetLength(0); i++)
+            {
+                for(int j = 0; j< matrix2.GetLength(1); j ++)
+                {
+                    temp[tempi] = matrix2[i, j];
+                    tempi++;
+                }
+            }
+
+            minEl = temp[0];
+            maxEl = temp[0];
+            for(int i = 0; i < temp.Length; i++)
+            {
+                if(minEl > temp[i])
+                {
+                    minEl = temp[i];
+                    minPos = i;
+                }
+                if (maxEl < temp[i])
+                {
+                    maxEl = temp[i];
+                    maxPos = i;
+                }
+            }
+
+            Console.WriteLine($"min el {minEl}, min pos {minPos}, max el {maxEl}, max pos {maxPos}");
+
+            if(minPos > maxPos)
+            {
+                for(int i = maxPos; i <= minPos; i++)
+                {
+                    summEl += temp[i];
+                }
+            }
+            else
+            {
+                for (int i = minPos; i <= maxPos; i++)
+                {
+                    summEl += temp[i];
+                }
+            }
+
+            Console.WriteLine($"The summ of the elements is {summEl}");
+
             Console.ReadKey();
 
+            
+
+
+            //task3
+
+
+            
+            string stringForEncode;
+            string encodedString = "";
+            char temp;
+
+            Console.WriteLine("Please enter string for encode:");
+            stringForEncode = Console.ReadLine();
+
+            for(int i =0; i < stringForEncode.Length; i++)
+            {
+                temp = stringForEncode[i];
+                temp = (char)(temp + 3);
+                encodedString += temp;
+            }
+            Console.WriteLine();
+            Console.WriteLine(encodedString);
+
+
+            Console.WriteLine("Encoding:");
+            for (int i = 0; i < encodedString.Length; i++)
+            {
+                temp = encodedString[i];
+                temp = (char)(temp - 3);
+                Console.Write(temp);
+            }
+
+            Console.ReadKey();
+            
+
+            //task 4
+
+            void multMatrix(int [][] matrix, char simbol, int number, int [][] matrix2 = null)
+            {
+                if(simbol == '*')
+                {
+                    for(int i =0; i < matrix.Length; i++)
+                    {
+                        for(int j = 0; j < matrix[i].Length; j++)
+                        {
+                            matrix[i][j] = matrix[i][j] * number;
+                        }
+                    }
+                }
+                else if(simbol =='+')
+                {
+                    for (int i = 0; i < matrix.Length; i++)
+                    {
+                        for (int j = 0; j < matrix[i].Length; j++)
+                        {
+                            matrix[i][j] = matrix[i][j] + matrix2[i][j];
+                        }
+                    }
+                }
+                else if(simbol == 'm')
+                {
+                    for (int i = 0; i < matrix.Length; i++)
+                    {
+                        for (int j = 0; j < matrix[i].Length; j++)
+                        {
+                            matrix[i][j] = matrix[i][j] * matrix2[i][j];
+                        }
+                    }
+                }
+
+            }
+
+            //making matrix1, 2
+
+            int[][] myMatrix = new int[3][];
+            for(int i = 0; i < myMatrix.Length; i++)
+            {
+                myMatrix[i] = new int[4];
+            }
+
+            int[][] myMatrix2 = new int[3][];
+            for (int i = 0; i < myMatrix2.Length; i++)
+            {
+                myMatrix2[i] = new int[4];
+            }
+
+            //adding numbers to matrix1, 2
+            for (int i = 0; i < myMatrix.Length; i++)
+            {
+                for(int j = 0; j < myMatrix[i].Length; j++)
+                {
+                    myMatrix[i][j] = r.Next(1, 12);
+                }
+            }
+
+            for (int i = 0; i < myMatrix2.Length; i++)
+            {
+                for (int j = 0; j < myMatrix2[i].Length; j++)
+                {
+                    myMatrix2[i][j] = r.Next(1, 12);
+                }
+            }
+
+            //matrix before
+            Console.WriteLine("Before: ");
+            for (int i = 0; i < myMatrix.Length; i++)
+            {
+                for (int j = 0; j < myMatrix[i].Length; j++)
+                {
+                    Console.Write($"{myMatrix[i][j]} ");
+                }
+                Console.WriteLine();
+            }
+
+            //mult matrix to two
+
+            multMatrix(myMatrix, '*', 2);
+
+            //matrix after
+            Console.WriteLine("After *2: ");
+            for (int i = 0; i < myMatrix.Length; i++)
+            {
+                for (int j = 0; j < myMatrix[i].Length; j++)
+                {
+                    Console.Write($"{myMatrix[i][j]} ");
+                }
+                Console.WriteLine();
+            }
+
+            multMatrix(myMatrix, '+', 3, myMatrix2);
+
+            Console.WriteLine("After +: ");
+            for (int i = 0; i < myMatrix.Length; i++)
+            {
+                for (int j = 0; j < myMatrix[i].Length; j++)
+                {
+                    Console.Write($"{myMatrix[i][j]} ");
+                }
+                Console.WriteLine();
+            }
+
+            multMatrix(myMatrix, 'm', 3, myMatrix2);
+
+            Console.WriteLine("After m: ");
+            for (int i = 0; i < myMatrix.Length; i++)
+            {
+                for (int j = 0; j < myMatrix[i].Length; j++)
+                {
+                    Console.Write($"{myMatrix[i][j]} ");
+                }
+                Console.WriteLine();
+            }
+
+
+            Console.ReadKey();
         }
     }
 }
